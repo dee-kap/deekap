f5b6abd71801d2e18df457ab0bd5b05f994cc16e
Deepak <dee-kap@users.noreply.github.co>
2024-10-08 19:01:32 +1100

    update config

--- publishconf.py
+++ publishconf.py
@@ -15,6 +15,9 @@ FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
+PATH = "content"
+OUTPUT_PATH = "output/"
+
 
 # Following items are often useful when publishing
 
